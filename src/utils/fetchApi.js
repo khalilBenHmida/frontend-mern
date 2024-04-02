e51aab001c85716dd445c757219216c5b1072a5f
khalilBenHmida <khalilbusiness1000@gmail.com>
2024-04-02 13:55:23 +0100

    frontend

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://backend-mern-uep0.onrender.com"
 
 export const request = async (url, method, headers = {}, body = {}, isNotStringified = false) => {
     let res
